ec250c4b45673cefd571b2ea9e9bdeac21cfa732
EL BANNA <elbanna1@hotmail.fr>
2021-02-27 02:15:53 +0100

    Add styling with Tailwind css

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,11 +1,18 @@
 @extends('app')
 
 @section('title', 'About Us |' .config('app.name'))
-
-<img src="{{asset('imageslaravel/paris.jpg') }}" alt="paris">
 @section('content')
- <p>Built with &hearts; by LES TEACHERS DU NET.</p>
+<img src="{{asset('imageslaravel/paris.jpg') }}" alt="paris" class="my-12 rounded-full 
+shadow-md">
 
-  <p><a href="{{route('home')}}">Revenir à la page d'accueil</a></p>
+
+ <h2 class="mb-5 text-gray-700">
+      Built with <span class="text-pink-600">&hearts;</span> by LES TEACHERS DU NET.
+ </h2>
+
+  <p>
+  <a href="{{route('home')}}" class="text-indigo-400 hover:text-indigo-700 underline">
+  Revenir à la page d'accueil</a>
+  </p>
 @endsection
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,13 +1,14 @@
-    @extends('app')
+@extends('app')
 
 @section('title', env('APP_NAME'))
+@section('content')
 
-<img src="{{asset('imageslaravel/flagFrance.jpg')}}" alt="franceFlag">
+       <img src="{{asset('imageslaravel/flagFrance.jpg')}}" alt="franceFlag" class=rounded my-12 shadow-md h-32>
 
-    @section('content')
 
-               <h1>Hello from Paris!</h1>
 
-               <p>It's currently {{ date('h:i A' ) }}.</p>
+       <h1 class ="mt-5 text-3xl sm:text-5xl  font-semibold text-indigo-600">Hello from Paris!</h1>
+
+        <p class="mb-5 text-lg text-gray-800">It's currently {{ date('h:i A' ) }}.</p>
    
-     @endsection
+@endsection
